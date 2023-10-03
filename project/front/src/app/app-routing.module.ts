506f1297c27ff86b30bb7e045916713854e8b528
David Ortega <davidortegacadena@gmail.com>
2023-10-02 22:51:32 -0500

    ajustes varios

--- project/front/src/app/app-routing.module.ts
+++ project/front/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  //Wild Card Route for 404 request
+  {
+    path: '**', pathMatch: 'full',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/front/src/app/app.module.ts
+++ project/front/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateComponent } from './components/clients/create/create.component';
+import { EditComponent } from './components/clients/edit/edit.component';
+import { IndexComponent } from './components/clients/index/index.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SidebarComponent } from './components/estructura/sidebar/sidebar.component';
+import { HeroComponent } from './components/estructura/hero/hero.component';
+import { FooterComponent } from './components/estructura/footer/footer.component';
+import { HeaderComponent } from './components/estructura/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    HomeComponent,
+    CreateComponent,
+    EditComponent,
+    IndexComponent,
+    RegisterComponent,
+    SidebarComponent,
+    HeroComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
